720b42406d133dbdbb2baaa6aaeb5e39622ea85e
Tenzil <lourdino.tenzil@imaginea.com>
2018-10-27 14:11:07 +0530

    Default application with gem

--- app/jobs/sample.rb
+++ app/jobs/sample.rb
@@ -7,19 +7,3 @@ Dashing.scheduler.every '20s' do
   Dashing.send_event('valuation', { current: current_valuation, last: last_valuation })
   Dashing.send_event('synergy',   { value: rand(100) })
 end
-
-
-
-@BBC_News = BbcNews.new()
-
-
-Dashing.scheduler.every '59s', :first_in => 0 do |job|
-  headlines = @BBC_News.latest_headlines
-  Dashing.send_event('news', { :headlines => headlines})
-end
-
-@questions = StackOverflow.new
-Dashing.scheduler.every '15s', :first_in => 0 do |job|
-  headlines = @questions.list_questions
-  Dashing.send_event('myrepos', { :headlines => headlines})
-end
